f23aa86b7035e2cf7cad07abaf1e2b7b00befc04
Claudia Alpert <claudiaalpert@gmail.com>
2021-02-06 11:33:33 -0600

    added player.js

--- src/components/App.js
+++ src/components/App.js
@@ -1,10 +1,14 @@
 import '../App.css';
 import Welcome from './Welcome'
+import { playerData } from '../data/playerData'
+import Player from './Player'
 
+console.log('pd', playerData)
 function App() {
   return (
   <div className="container">
     <Welcome />
+    <Player />
   </div>
   );
 }
